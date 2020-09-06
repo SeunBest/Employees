c10210fc20c70db29ee987860bc3c36031a84b9e
Oluwaseun Oyetoyan <oluwaseun.oyetoyan@decagon.dev>
2020-09-06 22:31:29 +0100

    Set up context

---
+++ Property Sets/PersonContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Property_Sets
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=CompanyDB;Trusted_Connection=True;");
+        }
+    }
+}
